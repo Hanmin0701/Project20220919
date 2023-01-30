4b2afed7135d3035079f5e302350d1e93fd6abb7
test <test@gmail.com>
2023-01-30 16:01:03 +0900

    페이징 완료 내용 추가

--- src/main/java/com/test/config/WebMvcConfig.java
+++ src/main/java/com/test/config/WebMvcConfig.java
@@ -1,13 +1,19 @@
 package com.test.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.test.common.fileManagerService;
+import com.test.interceptor.PermissionInterceptor;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer{
+	
+	@Autowired
+	private PermissionInterceptor interceptor;
 	
 	// 파일 연동해주는 방법
 	@Override
@@ -18,5 +24,12 @@ public class WebMvcConfig implements WebMvcConfigurer{
 		.addResourceLocations("file:///" + fileManagerService.FILE_UPLOAD_PATH); // 실재 파일 위치  
 		// mac = .addResourceLocations("file://");
 		// window = .addResourceLocations("file:///");
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(interceptor)
+		.addPathPatterns("/**")        //     /** 아래 디렉토리까지 확인
+		.excludePathPatterns("/favicon.ico", "/error", "/static/**", "/user/sign_out");
 	}
 }
--- src/main/java/com/test/post/bo/PostBO.java
+++ src/main/java/com/test/post/bo/PostBO.java
@@ -1,5 +1,6 @@
 package com.test.post.bo;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -22,6 +23,8 @@ public class PostBO {
 	// private Logger logger = LoggerFactory.getLogger(PostBO.class);
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
+	private static final int POST_MAX_SIZE = 3;
+	
 	@Autowired
 	private CommentBO commentBO;
 	
@@ -37,6 +40,8 @@ public class PostBO {
 	@Autowired
 	private fileManagerService fileManagerService;
 	
+	
+	// 글 추가
 	public int addPost(int userId, String userLoginId, String subject, String content, MultipartFile file) {
 		
 		// 파일 업로드 => 경로 설정
@@ -103,8 +108,43 @@ public class PostBO {
 		return postDAO.deletePostByPostIdUserId(postId, userId);
 	}
 	
-	public List<Post> getPostListByUserId(int userId) {
-		return postDAO.selectPostListByUserId(userId);
+	public List<Post> getPostListByUserId(int userId, Integer prevId, Integer nextId) {
+		// 게시글 번호:   10 9 8 | 7 6 5 | 4 3 2 | 1
+		// 만약 4 3 2 페이지에 있을 때
+		//  1) 이전: 정방향ASC 4보다 큰 3개(5 6 7) => List reverse(7 6 5)
+		//  2) 다음: 2보다 작은 3개 DESC
+		//  3) 첫페이지(이전, 다음 없음) DESC 3개
+		String direction = null; // 방향
+		Integer standardId = null; // 기준 postId
+		if (prevId != null) { // 이전
+			direction = "prev";
+			standardId = prevId;
+			
+			List<Post> postList = postDAO.selectPostListByUserId(userId, direction, standardId, POST_MAX_SIZE);
+			Collections.reverse(postList);
+			return postList;
+		} else if (nextId != null) { // 다음
+			direction = "next";
+			standardId = nextId;
+		}
+		
+		// 첫페이지일 때(페이징X) standardId, direction이 null
+		// 다음일 때 standarId, direction 채워져서 넘어감
+		return postDAO.selectPostListByUserId(userId, direction, standardId, POST_MAX_SIZE);
+	}
+	
+	public boolean isPrevLastPage(int prevId, int userId) {
+		int maxPostId = postDAO.selectPostIdByUserIdSort(userId, "DESC");
+		return maxPostId == prevId ? true : false;
+	}
+	
+	public boolean isNextLastPage(int nextId, int userId) {
+		int minPostId = postDAO.selectPostIdByUserIdSort(userId, "ASC");
+		return minPostId == nextId ? true : false;
+	}
+	
+	public List<Post> getPostListByUserId(int userId, String direction, Integer standardId, int limit) {
+		return postDAO.selectPostListByUserId(userId, null, null, userId);
 	}
 	
 	public Post getPostByPostIdUserId(int postId, int userId) {
